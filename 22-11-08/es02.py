af57d1d88ad529063aa19572c6f29119e22ec614
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-08 16:21:07 +0100

    update con set nel main

--- 22-11-08/es02.py
+++ 22-11-08/es02.py
@@ -8,10 +8,10 @@ def load_file_into_string(path_of_file):
     my_line = ""
     with open(path_of_file) as my_file:
         while True:                         #CS per ciclare la lettura di più righe e terminare alla linea vuota post-ultima riga 
-            my_tmp_line += my_file.readline()
-            if my_line == "":
+            my_tmp_line = my_file.readline()    #uso la temporanea per ciclare il controllo
+            if my_tmp_line == "":               #condizione linea vuota per il ciclo di controllo
                 break
-            my_line = my_tmp_line
+            my_line = my_tmp_line               #uguaglianza a my_line della temp dal controllo
     return my_line
 
 def extract_list_from_string(my_str:str, sep = " ", items_to_be_removed = [".",",",";",":","!","?"]):
@@ -24,4 +24,11 @@ def extract_list_from_string(my_str:str, sep = " ", items_to_be_removed = [".","
 
 def main():
     my_list1 = extract_list_from_string(load_file_into_string("./22-11-08/word1.txt"))
+    print(my_list1)
+    my_set1 = extract_list_from_string(load_file_into_string("./22-11-08/word1.txt"))
+    my_set2 = extract_list_from_string(load_file_into_string("./22-11-08/word2.txt"))
+    print(len(my_set1), len(my_set2))
+    my_common_words = my_set1.intersection(my_set2)
+    print(f"numero di parole comuni {len(my_common_words)}")
+    print(f"le parole in comune sono:\n {(my_common_words)}")
 main()
