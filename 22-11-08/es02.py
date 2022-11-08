bc046cc4051919851f8f1188336923a0b739355a
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-08 16:04:15 +0100

    update lettura ricorsiva

--- 22-11-08/es02.py
+++ 22-11-08/es02.py
@@ -7,7 +7,11 @@ def load_file_into_string(path_of_file):
     """Returns a string from a file"""
     my_line = ""
     with open(path_of_file) as my_file:
-        my_line += my_file.readline()
+        while True:                         #CS per ciclare la lettura di più righe e terminare alla linea vuota post-ultima riga 
+            my_tmp_line += my_file.readline()
+            if my_line == "":
+                break
+            my_line = my_tmp_line
     return my_line
 
 def extract_list_from_string(my_str:str, sep = " ", items_to_be_removed = [".",",",";",":","!","?"]):
@@ -19,5 +23,5 @@ def extract_list_from_string(my_str:str, sep = " ", items_to_be_removed = [".","
     return tmp_value.split(sep)
 
 def main():
-    my_list1 = extract_list_from_string(load_file_into_string())
+    my_list1 = extract_list_from_string(load_file_into_string("./22-11-08/word1.txt"))
 main()
