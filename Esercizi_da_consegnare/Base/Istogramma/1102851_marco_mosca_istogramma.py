4683aaa454557b1d1c45692e420169aa4056e780
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-15 14:43:01 +0100

    Update con chiamate di funzione

--- Esercizi_da_consegnare/Base/Istogramma/1102851_marco_mosca_istogramma.py
+++ Esercizi_da_consegnare/Base/Istogramma/1102851_marco_mosca_istogramma.py
@@ -1,55 +1,65 @@
 #
 #TODO: Calcolare min, max, media, deviazione standard di una serie di input dati dall'utente usando le liste
 #TODO: Opzionale: Diviso l'intervallo di valori dati dall'utente in N classi (input utente), calcolare la frequenza di ognuno di essi e stampare l'istogramma
-
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-N_DIV = int(input("Definire il numero di classi in cui dividere l'intervallo di numeri inserito..."))
-value_to_add = int(input("Definire quanti valori inserire..."))     #TODO prova ad eliminare inserendo il while true sotto a #L12
-my_list =[]
+def values(my_list):
+    """Function that determines the min, max, avg and std_dev of the user list"""
+    min_value = min(my_list)
+    max_value = max(my_list)
+    mean_value = np.mean(my_list)
+    devstd_value = np.std(my_list)
+    print(f"Il valore minimo della lista di valori inserita è {min_value:.3f},\n\
+            il valore massimo è {max_value:.3f},\nil valor medio è {mean_value:.3f},\n\
+            la deviazione standard {devstd_value:.3f}")
+    char_values_list = []
+    char_values_list.append(min_value)
+    char_values_list.append(max_value)
+    return char_values_list
 
-while len(my_list) != value_to_add:                                 #TODO Prova true
-    my_value = input("Inserire un numero o la parola \"exit\" per uscire dall'inserimento: \n")
-    if my_value != "exit":
-        try:
-            my_list.append(float(my_value))
-        except Exception as ex:
-            print("Inserimento non corretto:\nInserire un numero o la parola \"exit\"")
-    elif my_value == "exit":
-        break
+def bin_freq(my_list, bin_extr):
+    """Prints the frequency of each histogram class"""
+    my_list.sort()
+    sub_ints_freq = []#lista che contiene le len delle classi in sotto-liste
+    bin_extr.pop(0)
+    for element in my_list:
+        bin_list = []
+        if element <= bin_extr[0]:
+            bin_list.append(element)                                             #! appendere sottoliste per poi printarne il len
+            my_list.pop(0)
+        while element > bin_extr[0]:
+            bin_list.append(element)                                             #! appendere sottoliste per poi printarne il len
+            my_list.pop(0)
+            bin_extr.pop(0)
+        else:
+            sub_ints_freq.append(len(bin_list))
+    print(sub_ints_freq)
+    print(f"Le frequenze delle classi sono, dalla prima all'ultima, {sub_ints_freq}") #! da correggere e mettere len delle sottoliste
 
-print(f"I numeri inseriti sono\n{my_list}")
-
-min_value = min(my_list)
-max_value = max(my_list)
-mean_value = np.mean(my_list)
-devstd_value = np.std(my_list)
-print(f"Il valore minimo della lista di valori inserita è {min_value:.3f},\nil valore massimo è {max_value:.3f},\nil valor medio è {mean_value:.3f},\nla deviazione standard {devstd_value:.3f}")
-
-#* plot dell'istogramma
-sub_ints_largh = (max_value-min_value)/N_DIV                                    # larghezza delle classi
-sub_ints_bin_range = []                                                         # estremi delle classi
-
-for i in range(N_DIV+1):
-    sub_ints_bin_range.append(min_value+i*sub_ints_largh)                       # append degli estremi
-plt.hist(my_list, bins=sub_ints_bin_range, edgecolor='black')                   # plot istogramma con .hist, che prende in input i valori della lista e le divisioni in classi
-plt.show
-
-#! print della frequenza di ogni classe
-my_list.sort()
-sub_ints_freq = []#lista che contiene le len delle classi in sotto-liste
-sub_ints_bin_range.pop(0)
-for element in my_list:
-    bin_list = []
-    if element <= sub_ints_bin_range[0]:
-        bin_list.append(element)                                             #! appendere sottoliste per poi printarne il len
-        my_list.pop(0)
-    if element > sub_ints_bin_range[0]:
-        sub_ints_bin_range.pop(0)
-        bin_list.append(element)                                             #! appendere sottoliste per poi printarne il len
-        my_list.pop(0)
-    else:
-        sub_ints_freq.append(len(bin_list))
-print(sub_ints_freq)
-print(f"Le frequenze delle classi sono, dalla prima all'ultima, {sub_ints_freq}") #! da correggere e mettere len delle sottoliste
+def main():
+    """Entry point"""
+    my_list =[]
+    if len(sys.argv)==17:
+        value_to_add = len(sys.argv)
+        N_DIV = int(sys.argv[1])
+        for i in range (1,value_to_add):
+            my_value = input(sys.argv[i])
+            if my_value != "exit":
+                try:
+                    my_list.append(float(my_value))
+                except Exception as ex:
+                    print("Inserimento non corretto:\nInserire un numero o la parola \"exit\"")
+            elif my_value == "exit":
+                break
+    print(f"I numeri inseriti sono\n{my_list}")
+    list_val=values(my_list)
+    #* plot dell'istogramma
+    sub_ints_largh = (list_val[1]-list_val[0])/N_DIV                                   # larghezza delle classi
+    bin_extr = []                                                         # estremi delle classi
+    for i in range(N_DIV+1):
+        bin_extr.append(list_val[0]+i*sub_ints_largh)                       # append degli estremi
+    plt.hist(my_list, bins=bin_extr, edgecolor='black')                   # plot istogramma con .hist, che prende in input i valori della lista e le divisioni in classi
+    plt.show
+main()
