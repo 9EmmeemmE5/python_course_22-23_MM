b42d96cc90af8200c5f5f91f927573e3ab00f45b
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-15 15:37:37 +0100

    update 22-11-15 h1537

--- 22-11-15/es02_openwwatherAPI.py
+++ 22-11-15/es02_openwwatherAPI.py
@@ -1,1 +1,12 @@
 """OpenweatherAPI"""
+# per abilitare la virtual environment occorre digitare sul terminale "virtualenv venv" per creare 
+# il virtual env, mentre per attivarlo occorre digitare "venv\Scripts\activate", usato per non 
+# intaccare l'ambiente locale, NB occorre prima installarlo con pip
+
+import json
+import os
+import requests
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
