ad1394ac832e85906253e166c9cc06ce99cf2631
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-15 16:30:09 +0100

    intermediate update commit

--- 22-11-15/es02_openwwatherAPI.py
+++ 22-11-15/es02_openwwatherAPI.py
@@ -10,3 +10,73 @@ import numpy as np
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+
+#import API key
+open_weather_key = '786747fbd17266909639c55958338c21'
+cities=['Montegranaro','Turin','Naples','Milan','Bologna','Venice','Rome']
+
+#define base and query URL
+url = "https://api.openweathermap.org/data/2.5/weather?"
+query_url = f"{url}appid={open_weather_key}&q={cities[0]}"
+#aggiungo i dati relativi alla query di ricerca per la call API
+print(query_url)
+
+#get response
+response = requests.get(query_url).json() #chiedo il response della API in formato JSON
+print(response)
+
+#create summary dict: creo le key del dizionario
+data = {
+    "City":[],
+    "Cloudiness":[],
+    "Conutry":[],
+    "Data":[],
+    "Humidity":[],
+    "Lat":[],
+    "Long":[],
+    "Max temp":[],
+    "Wind speed":[],
+}
+
+#set counts
+number = 0
+sets = 1
+
+#loop through cities in list
+for item in cities:
+    try:
+        query_url = f"{url}appid={open_weather_key}&q={item}"   #try della chiamata
+        response =requests.get(query_url).json()                #fetch del response
+        number=+1                                        #iter count +1
+        print(f"Processing record {number} of Sets {sets} | {item}") #print del 
+        data["City"].append(response['name'])
+        data["Cloudiness"].append(response['clouds']['all'])
+        data["Country"].append(response['sys']['country'])
+        data["Date"].append(response['dt'])
+        data["Humidity"].append(response['main']['humidity'])
+        data["Lat"].append(response['coord']['lat'])
+        data["Long"].append(response['coord']['lon'])
+        data["Max temp"].append(response['main']['temp_max'])
+        data["Wind speed"].append(response['main']['speed'])
+    except:
+        print("City not found; Skipping...")
+
+#limit API requests in order to not exceed sub limit
+time.sleep(5) #intervallo di 5 sec tra un push call and fetch info
+
+print(data)
+city_data_df=pd.DataFrame(data) #dataframe dalla libreria pandas aka tabella strutturata per convertire il nostro csv 
+max_temp = city_data_df["Max Temp"]
+city = city_data_df["City"]
+
+#set date
+city_data_df['Date']=pd.to_datetime(city_data_df['Date'], unit='s')
+date=city_data_df["Date"][0]
+
+#create plt
+plt.scatter(city, max_temp, s=40, c="slateblue", edgecolors="black", alpha=)
+plt.title(f"City latitude vs Temperature({date})")
+plt.xlabel(Latitude)
+plt.ylabel(Max Temp(F))
+plt.grid(b=none, which=major, axis=both)
+plt.show()
