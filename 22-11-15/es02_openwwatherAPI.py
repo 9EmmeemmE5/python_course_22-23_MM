c6ea8d2d5ea37d3818b75ecd48eb3ef2c48dd42a
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-16 15:38:24 +0100

    Update lezione 22-16-11 h1538

--- 22-11-15/es02_openwwatherAPI.py
+++ 22-11-15/es02_openwwatherAPI.py
@@ -17,7 +17,7 @@ cities=['Roma','Torino','Napoli','Venezia','Bologna','Milano']
 
 #define base and query URL
 url = "https://api.openweathermap.org/data/2.5/weather?"
-query_url = f"{url}appid={open_weather_key}&q={cities[0]}"
+query_url = f"{url}appid={open_weather_key}&q={cities[0]}&units=metric"
 #aggiungo i dati relativi alla query di ricerca per la call API
 # print(query_url)
 
@@ -25,17 +25,17 @@ query_url = f"{url}appid={open_weather_key}&q={cities[0]}"
 response = requests.get(query_url).json() #chiedo il response della API in formato JSON
 print(response)
 
-#create summary dict: creo le key del dizionario
-data = {
-    "City":[],
-    "Cloudiness":[],
-    "Conutry":[],
-    "Data":[],
-    "Humidity":[],
-    "Lat":[],
-    "Long":[],
-    "Max temp":[],
-    "Wind speed":[],
+#crate summary dict
+data = { 
+    "City": [],
+    "Cloudiness": [],
+    "Country": [],
+    "Date": [],
+    "Humidity": [],
+    "Lat": [],
+    "Long": [],
+    "Max Temp": [],
+    "Wind Speed": []
 }
 
 #set counts
@@ -45,7 +45,7 @@ sets = 1
 #loop through cities in list
 for item in cities:
     try:
-        query_url = f"{url}appid={open_weather_key}&q={item}"   #try della chiamata
+        query_url = f"{url}appid={open_weather_key}&q={item}&units=metric"   #try della chiamata
         response =requests.get(query_url).json()                #fetch del response
         number=+1                                        #iter count +1
         print(f"Processing record {number} of Sets {sets} | {item}") #print del 
@@ -56,8 +56,8 @@ for item in cities:
         data["Humidity"].append(response['main']['humidity'])
         data["Lat"].append(response['coord']['lat'])
         data["Long"].append(response['coord']['lon'])
-        data["Max temp"].append(response['main']['temp_max'])
-        data["Wind speed"].append(response['main']['speed'])
+        data["Max Temp"].append(response['main']['temp_max'])
+        data["Wind Speed"].append(response['wind']['speed'])
     except:
         print("City not found; Skipping...")
 
@@ -66,6 +66,8 @@ time.sleep(5) #intervallo di 5 sec tra un push call and fetch info
 
 print(data)
 city_data_df=pd.DataFrame(data) #dataframe dalla libreria pandas aka tabella strutturata per convertire il nostro csv 
+
+#plot max temp
 max_temp = city_data_df["Max Temp"]
 city = city_data_df["City"]
 
@@ -78,5 +80,18 @@ plt.scatter(city,max_temp, s=40, c="slateblue", edgecolors="black", alpha=.75)
 plt.title(f"City Latitude vs Temperature({date})")
 plt.xlabel("Latitude")
 plt.ylabel("Max Temp (F)")
+plt.grid(b=None,which='major',axis='both')
+plt.show()
+
+#plot humidity
+humidity=data['Humidity'] #basta richiamare la chiave humidity
+city_data_df['Date']=pd.to_datetime(city_data_df['Date'], unit='s')
+date=city_data_df['Date'][0]
+
+#create plot
+plt.scatter(city,humidity, s=40, c="slateblue", edgecolors="black", alpha=.75)
+plt.title(f"City Latitude vs Humidity")
+plt.xlabel("City")
+plt.ylabel("Humidity %")
 plt.grid(b=None,which='major',axis='both')
 plt.show()
