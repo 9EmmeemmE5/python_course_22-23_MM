e25a7106e478e6beb47df51bdb608e3432f0d299
9EmmeemmE5 <75636953+9EmmeemmE5@users.noreply.github.com>
2022-11-16 13:05:54 +0100

    Update 22-11-16 h1305

--- 22-11-15/es01_dizionari.py
+++ 22-11-15/es01_dizionari.py
@@ -101,45 +101,62 @@ set1 = set(list1)
 print(list(set1))
 
 
-# 5
+#* 5
 # Write a Python program to create a new dictionary, 
 # named newDict, by extracting the mentioned 
 # keys= ["name", "salary"] from the following dictionary:
 
 sampleDict = {"name": "Kelly","age": 25,"salary": 8000,"city": "New york"}
 keys = ["name", "salary"]
+newDict = {}
+newDict["name"]=sampleDict['name']
+newDict["salary"]=sampleDict['salary']
+newDict1={}
+for element in keys:
+    newDict1[element]=sampleDict[element]
 
-# 6 
+#* 6 
 # Delete using the following list of keys ["name", "salary"] 
 # from the previous sampleDict dictionary
 # OUTPUT: {'city': 'New york', 'age': 25}
 
+for element in keys:
+    sampleDict.pop(element)
 
 # 7
 # Given the following dictionary:
 inventory = {
     'gold' : 500,
     'pouch' : ['flint', 'twine', 'gemstone'],
-    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']}
-
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+    }
 
 #Try to do the followings:
 
-# a) Add a key to inventory called 'pocket'and set the value 
+#* a) Add a key to inventory called 'pocket'and set the value 
 # of 'pocket' to be a list consisting of the strings 'seashell', 
 # 'strange berry', and 'lint'.
+inventory['pocket']=["seashell","strange  berry","lint"]
 
+#* b) .sort() the items in the list stored under the 'backpack' key.
+# backpack=[]
+# backpack=inventory.get('backpack')
+# inventory.pop('backpack')
+# backpack.sort()
+# inventory['backpack']=backpack
+inventory['backpack']=sorted(inventory['backpack'])
 
-# b) .sort() the items in the list stored under the 'backpack' key.
+#* c) Then .remove('dagger') from the list of items stored under the 'backpack' key.
+backpack=inventory.get('backpack')
+inventory.pop('backpack')
+backpack.pop(2)
+inventory['backpack']=backpack
 
+#* d) Add 50 to the number stored under the 'gold' key.
+inventory['gold']=inventory['gold']+50
 
-# c) Then .remove('dagger') from the list of items stored under the 'backpack' key.
-
-
-# d) Add 50 to the number stored under the 'gold' key.
-
-
-# e) print(inventory)
-
+#* e) print(inventory)
+print(inventory)
 
 #f) Print in reverse order all values of dictionary with 'backpack' keys
+print(inventory.values(), reverse=True)
